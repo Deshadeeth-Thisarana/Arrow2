b47bcda6d60be642fd2f7d0b6ef3c5706713e693
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-06 17:33:44 +0530

    Update and rename cust_filters.py to filters.py

--- SaitamaRobot/modules/filters.py
+++ SaitamaRobot/modules/filters.py
@@ -596,32 +596,38 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/filters`*:* List all active filters saved in the chat.
+ • /filters*:* List all active filters saved in the chat.
 
 *Admin only:*
- • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
- Separate diff replies by `%%%` to get random replies
- *Example:* 
- `/filter "filtername"
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
- • `/stop <filter keyword>`*:* Stop that filter.
+
+• /filter `<keyword>` `<reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+  is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
+  keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
+  doin?
+  Separate diff replies by `%%%` to get random replies
+  
+  *Example:* 
+  `/filter "filtername"
+  Reply 1
+  %%%
+  Reply 2
+  %%%
+  Reply 3`
+ 
+ • /stop `<filter keyword>`*:* Stop that filter.
 
 *Chat creator only:*
- • `/removeallfilters`*:* Remove all chat filters at once.
+ 
+ • /removeallfilters*:* Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
 
+Reports bugs at @GangOfFriends
+
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "🎛Filters🎛"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
