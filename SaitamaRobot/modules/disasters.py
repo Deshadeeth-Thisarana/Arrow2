4885fa4c461e991fd847c956370f46f92c164826
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-06 17:07:30 +0530

    Update disasters.py

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -634,89 +634,86 @@ Commands listed here only work for users with special access are mainly used for
 Group admins/group owners do not need these commands. 
 
  ╔ *List all special users:*
- ╠ `/dragons`*:* Lists all Dragon disasters
- ╠ `/demons`*:* Lists all Demon disasters
- ╠ `/tigers`*:* Lists all Tigers disasters
- ╠ `/wolves`*:* Lists all Wolf disasters
- ╠ `/heroes`*:* Lists all Hero Association members
- ╠ `/adddragon`*:* Adds a user to Dragon
- ╠ `/adddemon`*:* Adds a user to Demon
- ╠ `/addtiger`*:* Adds a user to Tiger
- ╠ `/addwolf`*:* Adds a user to Wolf
+ ╠ /dragons*:* Lists all Dragon disasters
+ ╠ /demons*:* Lists all Demon disasters
+ ╠ /tigers*:* Lists all Tigers disasters
+ ╠ /wolves*:* Lists all Wolf disasters
+ ╠ /heroes*:* Lists all Hero Association members
+ ╠ /adddragon*:* Adds a user to Dragon
+ ╠ /adddemon*:* Adds a user to Demon
+ ╠ /addtiger*:* Adds a user to Tiger
+ ╠ /addwolf*:* Adds a user to Wolf
  ╚ `Add dev doesnt exist, devs should know how to add themselves`
 
  ╔ *Ping:*
- ╠ `/ping`*:* gets ping time of bot to telegram server
- ╚ `/pingall`*:* gets all listed ping times
+ ╠ /ping*:* gets ping time of bot to telegram server
+ ╚ /pingall*:* gets all listed ping times
 
  ╔ *Broadcast: (Bot owner only)*
  ╠  *Note:* This supports basic markdown
- ╠ `/broadcastall`*:* Broadcasts everywhere
- ╠ `/broadcastusers`*:* Broadcasts too all users
- ╚ `/broadcastgroups`*:* Broadcasts too all groups
+ ╠ /broadcastall*:* Broadcasts everywhere
+ ╠ /broadcastusers*:* Broadcasts too all users
+ ╚ /broadcastgroups*:* Broadcasts too all groups
 
  ╔ *Groups Info:*
- ╠ `/groups`*:* List the groups with Name, ID, members count as a txt
- ╠ `/leave <ID>`*:* Leave the group, ID must have hyphen
- ╠ `/stats`*:* Shows overall bot stats
- ╠ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
- ╚ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╠ /groups*:* List the groups with Name, ID, members count as a txt
+ ╠ /leave `<ID>`*:* Leave the group, ID must have hyphen
+ ╠ /stats*:* Shows overall bot stats
+ ╠ /getchats*:* Gets a list of group names the user has been seen in. Bot owner only
+ ╚ /ginfo `username/link/ID`*:* Pulls info panel for entire group
 
  ╔ *Access control:* 
- ╠ `/ignore`*:* Blacklists a user from 
- ╠  using the bot entirely
- ╠ `/notice`*:* Removes user from blacklist
- ╚ `/ignoredlist`*:* Lists ignored users
+ ╠ /ignore*:* Blacklists a user from using the bot entirely
+ ╠ /notice*:* Removes user from blacklist
+ ╚ /ignoredlist*:* Lists ignored users
 
  ╔ *Module loading:*
- ╠ `/listmodules`*:* Prints modules and their names
- ╠ `/unload <name>`*:* Unloads module dynamically
- ╚ `/load <name>`*:* Loads module
+ ╠ /listmodules*:* Prints modules and their names
+ ╠ /unload `<name>`*:* Unloads module dynamically
+ ╚ /load `<name>`*:* Loads module
 
  ╔ *Speedtest:*
- ╚ `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+ ╚ /speedtest*:* Runs a speedtest and gives you 2 options to choose from, text or image output
 
  ╔ *Global Bans:*
- ╠ `/gban user reason`*:* Globally bans a user
- ╚ `/ungban user reason`*:* Unbans the user from the global bans list
+ ╠ /gban `user` `reason`*:* Globally bans a user
+ ╚ /ungban `user `reason`*:* Unbans the user from the global bans list
 
  ╔ *Module loading:*
- ╠ `/listmodules`*:* Lists names of all modules
- ╠ `/load modulename`*:* Loads the said module to 
- ╠   memory without restarting.
- ╠ `/unload modulename`*:* Loads the said module from
- ╚   memory without restarting.memory without restarting the bot 
+ ╠ /listmodules*:* Lists names of all modules
+ ╠ /load modulename`*:* Loads the said module to memory without restarting.
+ ╚ /unload modulename`*:* Loads the said module from memory without restarting.memory without restarting the bot    
 
  ╔ *Remote commands:*
- ╠ `/rban user group`*:* Remote ban
- ╠ `/runban user group`*:* Remote un-ban
- ╠ `/rpunch user group`*:* Remote punch
- ╠ `/rmute user group`*:* Remote mute
- ╚ `/runmute user group`*:* Remote un-mute
+ ╠ /rban `user` `group`*:* Remote ban
+ ╠ /runban `user` `group`*:* Remote un-ban
+ ╠ /rpunch `user` `group`*:* Remote punch
+ ╠ /rmute `user` `group`*:* Remote mute
+ ╚ /runmute `user` `group`*:* Remote un-mute
 
  ╔ *Windows self hosted only:*
- ╠ `/reboot`*:* Restarts the bots service
- ╚ `/gitpull`*:* Pulls the repo and then restarts the bots service
+ ╠ /reboot*:* Restarts the bots service
+ ╚ /gitpull*:* Pulls the repo and then restarts the bots service
 
  ╔ *Chatbot:* 
- ╚ `/listaichats`*:* Lists the chats the chatmode is enabled in
+ ╚ /listaichats*:* Lists the chats the chatmode is enabled in
  
  ╔ *Debugging and Shell:* 
- ╠ `/debug <on/off>`*:* Logs commands to updates.txt
- ╠ `/logs`*:* Run this in support group to get logs in pm
- ╠ `/eval`*:* Self explanatory
- ╠ `/sh`*:* Runs shell command
- ╠ `/shell`*:* Runs shell command
- ╠ `/clearlocals`*:* As the name goes
- ╠ `/dbcleanup`*:* Removes deleted accs and groups from db
- ╚ `/py`*:* Runs python code
+ ╠ /debug `<on/off>`*:* Logs commands to updates.txt
+ ╠ /log`*:* Run this in support group to get logs in pm
+ ╠ /eval*:* Self explanatory
+ ╠ /sh*:* Runs shell command
+ ╠ /shell*:* Runs shell command
+ ╠ /clearlocals*:* As the name goes
+ ╠ /dbcleanup*:* Removes deleted accs and groups from db
+ ╚ /py*:* Runs python code
  
  ╔ *Global Bans:*
- ╠ `/gban <id> <reason>`*:* Gbans the user, works by reply too
- ╠ `/ungban`*:* Ungbans the user, same usage as gban
- ╚ `/gbanlist`*:* Outputs a list of gbanned users
+ ╠ /gban `<id>` `<reason>`*:* Gbans the user, works by reply too
+ ╠ /ungban*:* Ungbans the user, same usage as gban
+ ╚ /gbanlist*:* Outputs a list of gbanned users
 
-Visit @{SUPPORT_CHAT} for more information.
+Visit @GangOfFriends for more information.
 """
 
 DEV_HANDLER = CommandHandler(("addpiro", "addsudo"), addpiro)
@@ -757,7 +754,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Disasters"
+__mod_name__ = "👨‍🎨Disasters👨‍🎨"
 __handlers__ = [
     DEV_HANDLER, SUDO_HANDLER, SUPPORT_HANDLER, TIGER_HANDLER, WHITELIST_HANDLER,
     RMPIRO_HANDLER, UNSUDO_HANDLER, UNSUPPORT_HANDLER, UNTIGER_HANDLER, UNWHITELIST_HANDLER,
