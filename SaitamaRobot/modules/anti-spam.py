7ad547b173a0d3d5b56394d666b0cab3ac86d69b
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-20 07:21:35 +0530

    Update anti-spam.py

--- SaitamaRobot/modules/anti-spam.py
+++ SaitamaRobot/modules/anti-spam.py
@@ -527,7 +527,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "🈵Anti-Spam🈲"
+__mod_name__ = "Anti-Spam 🈲"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
