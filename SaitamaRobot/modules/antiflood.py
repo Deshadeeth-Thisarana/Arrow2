50f66422589944e068f6d3e5af049f476533f0b7
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-03-20 07:25:58 +0530

    Update antiflood.py

--- SaitamaRobot/modules/antiflood.py
+++ SaitamaRobot/modules/antiflood.py
@@ -406,9 +406,9 @@ will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
  • /flood*:* Get the current flood control setting
 • *Admins only:*
- • /setflood <int/'no'/'off'>*:* enables or disables flood control
+ • /setflood `<int/'no'/'off'>`*:* enables or disables flood control
  *Example:* `/setflood 10`
- • /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+ • `/setfloodmode` `<ban/kick/mute/tban/tmute>` `<value>`*:* Action to perform when user have exceeded flood limit. `ban/kick/mute/tmute/tban`
 • *Note:*
  • Value must be filled for tban and tmute!!
  It can be:
@@ -418,7 +418,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "🗣Anti-Flood🗣"
+__mod_name__ = "Anti-Flood 🗣"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
