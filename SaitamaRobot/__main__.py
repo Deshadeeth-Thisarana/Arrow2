28dde6ccd5ebbaf844568a82feab757c43e19734
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-08-17 12:39:37 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -347,8 +347,6 @@ def get_help(update: Update, context: CallbackContext):
            ),
         ),
             
-            return
-
  elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
         module = args[1].lower()
         text = "Here is the available help for the *{}* module:\n".format(HELPABLE[module].__mod_name__) \
